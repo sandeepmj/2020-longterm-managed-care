8da7128c3a73d620fcfdb3ce8fba05ed9efbd32e
sandeepmj <sjnews@gmail.com>
2020-03-06 07:37:10 -0800

    can find Alzheimer’s

--- script.py
+++ script.py
@@ -7,4 +7,12 @@ all_files = glob.glob(path + "/*.pdf")
 
 for file in all_files:
     text = extract_text(file)
-    print(text)
+    # print(file)
+    # print(text)
+    case = [sentence + '.' for sentence in text.split('.') if 'Alzheimer’s' in sentence]
+    print(type(case))
+    print(len(case))
+    if len(case) == 0:
+        pass
+    else:
+        print(f"{case} in {file}")
