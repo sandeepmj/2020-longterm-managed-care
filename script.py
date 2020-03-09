ca99d2e48e2496335b90b8bd45793cffd37018ac
sandeepmj <sjnews@gmail.com>
2020-03-09 11:53:52 -0400

    fixed date finder since request: existed otherplaces too

--- script.py
+++ script.py
@@ -12,19 +12,19 @@ count = 1
 tester = 0
 key_terms = ["alzheimer’s", "dementia"]
 dates_list = []
-occurence_list = []
+mentions_list = []
 cognition_list = []
 status_list = []
 positive_decision_list = []
 decision_date_list = []
-for file in all_files[11:100]:
+for file in all_files[0:10]:
 
     print(f" Processing {count} of {total} document: {file}")
     count += 1
     text = extract_text(file)
     text = text.lower()
     # print(text)
-    match = re.findall(('request: .+'), text)
+    match = re.findall(('request:\s\w+\s\d{0,2},\s\d{4}'), text)
     # date = match.replace('request:', "")
     date = [item.replace('request:', "").strip() for item in match]
     # print(f"date : {date}")
@@ -61,21 +61,32 @@ for file in all_files[11:100]:
 
     # print(file)
     # print(text)
+
     for sentence in file:
+        ignore = 'it provides, in relevant part:'
 #         print(sentence)
-        mentions = [sentence + '.' for sentence in text.split('.') if "dementia" in sentence or "alzheimer’s" in sentence]
-        cognition = [True]
-        occurence = [item.replace('\n\n', "").replace('\n', '').strip() for item in mentions]
+        mentions = [sentence + '.' for sentence in text.split('.') if "dementia" in sentence or "alzheimer’s" in sentence and "it provides, in relevant part:a state" not in sentence]
+        mentions = [item.replace('\n\n', "").replace('\n', '').strip() for item in mentions]
+
+        if ignore in mentions and len(mentions) >= 1:
+            cognition = [False]
+            mentions = ["NO OCCURENCES"]
+            # print(f"LALALALA {mentions}")
     #         print(case)
     #         print(len(case))
-        if len(occurence) == 0:
+        elif len(mentions) == 0:
             cognition = [False]
-            occurence = ["NO OCCURENCES"]
-    occurence_list.append(occurence)
-    cognition_list.append(cognition)
+            mentions = ["NO OCCURENCES"]
 
-# print(dates_list)
-print(occurence_list)
+        else:
+            cognition = [True]
+
+    mentions_list.append(mentions)
+    cognition_list.append(cognition)
+    # print(f"HELLLLLLO: {mentions}")
+
+print(dates_list)
+# print(mentions_list)
 # print(cognition_list)
 # print(positive_decision_list)
 # print(decision_date_list)
@@ -91,11 +102,11 @@ for sublist in dates_list:
 # def firstItems(lst):
 #     return [item[0] for item in lst]
 
-flat_occurence_list=[]
-for alist in occurence_list:
+flat_mentions_list=[]
+for alist in mentions_list:
     together = '..........'.join(alist)
-    flat_occurence_list.append(together)
-print(flat_occurence_list)
+    flat_mentions_list.append(together)
+# print(flat_mentions_list)
 
 # flat_occurence_list = firstItems(occurence_list)
 # print(flat_occurence_list)
@@ -108,16 +119,15 @@ flat_decision_date_list = [item for sublist in decision_date_list for item in su
 files_list = [item.replace("pdfs/Redacted_", "") for item in all_files[11:100]]
 
 decisions_dict_list = []
-for (file, date_a, date_d, cog, decision, text) in zip(files_list, appeal_dates_list, flat_decision_date_list, cognition_list, positive_decision_list, flat_occurence_list):
+for (file, date_a, date_d, cog, decision, text) in zip(files_list, appeal_dates_list, flat_decision_date_list, cognition_list, positive_decision_list, flat_mentions_list):
     each_decision = {"file_id": file, 'date_appeal': date_a, 'date_decision': date_d, 'cognition_related': cog, "positive_decision": decision, "dementia-related-words": text}
     decisions_dict_list.append(each_decision)
 # print(decisions_dict_list)
-# for (files_list, dates_list, decision_date_list, cognition_list, positive_decision_list)
-# print(decisions_dict_list)
+
 
 labels = ["file_id","date_appeal", "date_decision", "cognition_related", "positive_decision", "dementia-related-words" ]
 
-# csv file to be created with data
+## csv file to be created with data
 csv_file_name = "ltmc_decisions.csv"
 file_exists = os.path.isfile(csv_file_name)
 try:
